6a5aa094eb2ff41a7576f0eef4364be722218dde
Adriana <adriana@homolova.sk>
2014-09-23 14:12:28 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -6,8 +6,8 @@ url = "https://imgur.com/signin?minimal"
 browser = mechanize.Browser()
 browser.open(url)
 browser.select_form(nr = 0)
-browser.form['username'] = thisisnotarobot
-browser.form['password'] = lickmybattery
+browser.form['username'] = "thisisnotarobot"
+browser.form['password'] = "lickmybattery"
 browser.submit()
 
 for link in browser.links():\
