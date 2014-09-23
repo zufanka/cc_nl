6631c24d21c8472f73e2e803c25089f4cae99f2e
Adriana <adriana@homolova.sk>
2014-09-23 14:05:59 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,9 +1,11 @@
 from bs4 import BeautifulSoup as BS
 import mechanize
 
+url = "https://imgur.com/signin?minimal"
+
 browser = mechanize.Browser()
 browser.open()
 browser.select_form(nr = 0)
-browser.form['username'] = USERNAME
-browser.form['password'] = PASSWORD
+browser.form['username'] = thisisnotarobot
+browser.form['password'] = Lickmybattery
 browser.submit()
