057bcbac172fea14600c6a965f8285b0c3d805ec
Adriana <adriana@homolova.sk>
2014-09-23 15:05:45 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -7,9 +7,9 @@ def login(url):
   browser = mechanize.Browser()
   browser.open(url)
   
-  print [form for form in browser.forms()]
+  print [form for form in browser.forms()][0]
   
-  browser.select_form(nr = 0)
+  browser.select_form(nr = 1)
   browser.form['url'] = "thisisnotarobot"
   browser.form['password'] = "lickmybattery"
   
